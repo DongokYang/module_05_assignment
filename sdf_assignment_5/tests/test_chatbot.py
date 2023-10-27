0b2d5b8b0d6d3d4d8ae06ec3a8721544fa7af31b
DongokYang <dyang2@academic.rrc.ca>
2023-10-27 16:18:08 -0500

    Defined chatbotTests in test_chatbot.py

--- sdf_assignment_5/tests/test_chatbot.py
+++ sdf_assignment_5/tests/test_chatbot.py
@@ -1,9 +1,10 @@
 """
-Description:
-Author:
-Date:
+Description: 
+Author: Dongok Yang
+Date: 2023.10.27
 Usage:
 """
 import unittest
 from unittest.mock import patch
 
+class ChatbotTests(unittest.TestCase):
